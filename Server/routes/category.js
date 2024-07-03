9889f8f3996d8309d3fff62fe94185278b05bac9
vuducanh221104 <vuducanh22112004@gmail.com>
2024-07-03 19:47:56 +0700

    fix(user) :fix/update-category,product

--- Server/routes/category.js
+++ Server/routes/category.js
@@ -4,7 +4,10 @@ const CategoryController = require('../Controllers/CategoryController');
 
 router.get('/', CategoryController.categoryList);
 router.post('/', CategoryController.categoryAdd);
+router.patch('/', CategoryController.categoryUpdate);
+router.delete('/', CategoryController.categoryDelete);
 //Get To Slug And Query Material
+
 router.get('/:slug', CategoryController.categoryAndQueryMaterial);
 
 module.exports = router;
