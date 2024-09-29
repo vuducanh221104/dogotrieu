23d76ef42a4608b3c67537b1e81a5fd7e2666e61
vuducanh221104 <vuducanh22112004@gmail.com>
2024-09-29 20:31:57 +0700

    fix(user): npm install

--- Client/src/FireBase/config.tsx
+++ Client/src/FireBase/config.tsx
@@ -1,16 +1,20 @@
-import { initializeApp } from 'firebase/app';
-import { getFirestore } from 'firebase/firestore';
+// import { initializeApp } from 'firebase/app';
+// import { getFirestore } from 'firebase/firestore';
+function Test() {
+    return <></>;
+}
 
-const firebaseConfig = {
-    apiKey: 'AIzaSyBBjC8t4bTIop84ZDBL6eL3XGe54UAbosY',
-    authDomain: 'dogotrieu-40ab8.firebaseapp.com',
-    projectId: 'dogotrieu-40ab8',
-    storageBucket: 'dogotrieu-40ab8.appspot.com',
-    messagingSenderId: '1014195183139',
-    appId: '1:1014195183139:web:bcf9103eb37c22f9341b96',
-    measurementId: 'G-8T84XGM5PX',
-};
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
+export default Test;
+// const firebaseConfig = {
+//     apiKey: 'AIzaSyBBjC8t4bTIop84ZDBL6eL3XGe54UAbosY',
+//     authDomain: 'dogotrieu-40ab8.firebaseapp.com',
+//     projectId: 'dogotrieu-40ab8',
+//     storageBucket: 'dogotrieu-40ab8.appspot.com',
+//     messagingSenderId: '1014195183139',
+//     appId: '1:1014195183139:web:bcf9103eb37c22f9341b96',
+//     measurementId: 'G-8T84XGM5PX',
+// };
+// const app = initializeApp(firebaseConfig);
+// const db = getFirestore(app);
 
-export default db;
+// export default db;
